2d2a9621540ec9aeb72a56810d9756784c8129ac
ajand <iam@adrianjandongan.me>
2025-05-31 21:32:45 +0100

    Initial commit

---
+++ src/main/java/com/ncinft/vericert/NcinftVeriCertApplication.java
@@ -0,0 +1,13 @@
+package com.ncinft.vericert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcinftVeriCertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcinftVeriCertApplication.class, args);
+	}
+
+}
